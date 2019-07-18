3efb11b911f28505dc9e503a6d22c1a3010d571e
tplobbregt <tplobbregt@gmail.com>
2019-07-18 14:27:29 -0400

    init commit

---
+++ README.md
@@ -0,0 +1,100 @@
+<div align="center">
+<img src="./icons/icon.svg" alt="Logo"></a>
+</div>
+
+<h1 align="center">Topic Tabs</h1>
+<p align="center">
+Generates backgrounds for new tabs based on a user-provided topic <br>
+  Available for Chrome & Firefox
+</p> 
+<div align="center">
+  
+  [![Status](https://img.shields.io/badge/status-active-success.svg)]()
+  [![GitHub Issues](https://img.shields.io/github/issues/tplobbregt/topic-tabs.svg)](https://github.com/tplobbregt/topic-tabs/issues)
+  [![GitHub Pull Requests](https://img.shields.io/github/issues-pr/tplobbregt/topic-tabs.svg)](https://github.com/tplobbregt/topic-tabs/pulls)
+  [![License](https://img.shields.io/badge/license-MIT-blue.svg)](/LICENSE)
+  
+</div>
+
+---
+
+## Table of Contents
+
+- [About](#about)
+- [Demo](#demo)
+- [Tools](#tools)
+- [TODO](#todo)
+- [Getting Started](#getting_started)
+- [Authors](#authors)
+
+<a name = "about"></a>
+
+## About
+
+Topic Tabs is a browser extension that generates backgrounds for new tabs based on a user-provided topic. To change topics, simply enter the new topic in the provided input box at the top of each tab. The extension will then reach out to the Topic Tabs API, which will retrieve a new list of image data from Pexels.
+
+<a name = "demo"></a>
+
+## Demo
+
+<div align="center">
+<img src="./demo.png" alt="Demo"></a>
+</div>
+
+<a name = "tools"></a>
+
+## Tools
+
+- Extension
+
+  - HTML / CSS / Javascript
+
+- [Topic Tabs API](https://github.com/tplobbregt/topic-tabs-api)
+
+  - [Node.js](https://nodejs.org/en/about/) | JS runtime
+  - [Express](https://expressjs.com/) | Server framework
+
+- Deployment
+
+  - [Heroku](https://www.heroku.com/) | SaaS hosting platform for API
+
+- Data
+  - [Pexels API](https://www.pexels.com/api/) | Image and photographer information
+
+<a name = "todo"></a>
+
+## TODO
+
+- [ ] - Add to Chrome Web Store
+- [ ] - Add to Firefox Add-ons store
+
+<a name = "getting_started"></a>
+
+## Getting Started
+
+```bash
+# Clone repo
+git clone https://github.com/tplobbregt/topic-tabs
+```
+
+- Open Chrome extensions menu (chrome://extensions)
+- Turn on developer mode
+- Load unpacked extension
+
+Learn more about loading Chrome extensions [here](https://developer.chrome.com/extensions/getstarted).
+
+## App Info
+
+<a name = "authors"></a>
+
+### Author
+
+Tyler Lobbregt
+
+### Version
+
+1.0.0
+
+### License
+
+This project is licensed under the MIT License
---
+++ newTab/newTab.js
@@ -0,0 +1,92 @@
+const topicInput = document.getElementById('topic-input');
+const inputMsg = document.getElementById('input-msg');
+const mainImgBox = document.getElementById('main-img-box');
+const imgText = document.getElementById('img-txt');
+const imgPhotographer = document.getElementById('img-photographer');
+
+(() => {
+  checkNewTopic();
+  getTopic();
+  getImages();
+})();
+
+function checkNewTopic() {
+  topicInput.onkeydown = e => {
+    if (e.keyCode === 13) {
+      setTopic(event.target.value);
+      topicInput.value = event.target.value;
+      topicInput.disabled = true;
+      topicInput.style.cssText =
+        'color: rgb(71, 71, 71); border-color rgb(71, 71, 71);';
+    }
+  };
+}
+
+function getTopic() {
+  chrome.storage.local.get('topic', data => {
+    if (data.topic) {
+      const { topic } = data;
+      topicInput.placeholder = topic;
+    } else {
+      topicInput.placeholder = 'Enter a topic..';
+    }
+  });
+}
+
+function getImages() {
+  chrome.storage.local.get('images', data => {
+    if (data.images) {
+      const { images } = data;
+      const image = images[Math.floor(Math.random() * images.length)];
+      const mainImg = document.createElement('IMG');
+      mainImg.src = image.photo_url;
+      mainImg.id = 'main-img';
+      mainImgBox.appendChild(mainImg);
+      imgText.innerText = 'Photo by:';
+      imgPhotographer.innerText = ` ${image.photographer}`;
+      imgPhotographer.href = image.photographer_url;
+    }
+  });
+}
+
+async function setTopic(topic) {
+  try {
+    inputMsg.innerText = 'Loading...';
+    inputMsg.style.cssText = 'background-color: #007bff';
+
+    const resp = await fetch(
+      `https://topic-tabs-api.herokuapp.com/api/topic/?topic=${topic}`
+    );
+    const data = await resp.json();
+
+    if (data) {
+      const imageList = [];
+      data.photos.forEach(p => {
+        imageList.push({
+          photographer: p.photographer,
+          photographer_url: p.photographer_url,
+          photo_url: p.src.large2x,
+          pexel_url: p.url
+        });
+      });
+
+      chrome.storage.local.set({ topic: topic, images: imageList }, () => {
+        if (chrome.runtime.lastError) {
+          console.error(
+            `Error setting data to ${JSON.stringify(data)}: ${
+              chrome.runtime.lastError.message
+            }`
+          );
+        } else {
+          inputMsg.innerText = 'Loaded - Reload tab';
+          inputMsg.style.cssText = 'background-color: #28a745';
+        }
+      });
+    }
+  } catch (err) {
+    inputMsg.innerText = 'Unable to load topic';
+    inputMsg.style.cssText = 'background-color: #dc3545';
+    topicInput.disabled = false;
+    topicInput.style.cssText = 'color: #fff; border-color rgb(71, 71, 71);';
+  }
+}
